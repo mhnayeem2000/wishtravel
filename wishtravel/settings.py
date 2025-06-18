c4552ef4b2b50dfb22f2a7605cdff8dbd4c0ade5
mhnayeem2000 <mhnayeem018460@gmail.com>
2025-06-19 04:41:07 +0600

    update hero section

--- wishtravel/settings.py
+++ wishtravel/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-r*%+pje(wpwmq@!!c1%r5#73fvk^+&xzjwf789%w-jmzzs4hw+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
